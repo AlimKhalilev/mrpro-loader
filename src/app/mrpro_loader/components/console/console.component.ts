67cbd449c634cc78b2253fdd90f16645ced1b6cc
AlimKhalilev <alim-halilko@mail.ru>
2025-04-17 16:39:23 +0300

    [dev]: оптимизация console компонента

--- src/app/mrpro_loader/components/console/console.component.ts
+++ src/app/mrpro_loader/components/console/console.component.ts
@@ -1,20 +1,15 @@
-import { AfterContentInit, ChangeDetectionStrategy, Component, ContentChildren, ElementRef, QueryList } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ContentChildren, ElementRef, QueryList } from '@angular/core';
 import { LdrIconComponent } from '../icon/icon.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'ldr-console',
-	imports: [CommonModule, LdrIconComponent],
+	imports: [LdrIconComponent],
 	templateUrl: './console.component.html',
 	styleUrl: './console.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LdrConsoleComponent implements AfterContentInit {
+export class LdrConsoleComponent {
     @ContentChildren('ref', { descendants: true, read: ElementRef }) items: QueryList<ElementRef> | null = null;
-
-    ngAfterContentInit(): void {
-        console.log(this.items);
-    }
 
     getAllInnerText(elements: QueryList<ElementRef>): string {
         return Array.from(elements).map(element => element.nativeElement.innerText).join('\n');
