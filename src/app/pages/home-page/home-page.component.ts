7ee3e96ea83c79a28d65b87de9466d55c9cd3837
AlimKhalilev <alim-halilko@mail.ru>
2025-04-17 16:09:56 +0300

    [dev]: добавлены все существующие демки

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,10 +6,19 @@ import { LdrInfoBarComponent } from '../../mrpro_loader/components/info-bar/info
 import { LdrTextboxComponent } from '../../mrpro_loader/components/textbox/textbox.component';
 import { LdrToolbarButtonComponent } from '../../mrpro_loader/components/toolbar-button/toolbar-button.component';
 import { LdrWarningComponent } from '../../mrpro_loader/components/warning/warning.component';
+import { LdrConsoleComponent } from '../../mrpro_loader/components/console/console.component';
 
 @Component({
 	selector: 'ldr-home-page',
-	imports: [LdrButtonComponent, LdrInfoBarComponent, LdrToolbarButtonComponent, LdrTextboxComponent, LdrDetailsComponent, LdrWarningComponent, LdrWindowComponent],
+	imports: [
+		LdrButtonComponent,
+		LdrInfoBarComponent,
+		LdrTextboxComponent,
+		LdrDetailsComponent,
+		LdrWarningComponent,
+		LdrConsoleComponent,
+		LdrWindowComponent,
+	],
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush,
